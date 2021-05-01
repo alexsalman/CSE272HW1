930358aa0ed89fadac767234d2865f4a02634f07
Alex Salman <aalsalma@ucsc.edu>
2021-04-30 21:01:57 -0700

    indextionization

--- main.py
+++ main.py
@@ -3,29 +3,47 @@ import lucene
 from java.io import StringReader
 from org.apache.lucene.analysis.standard import StandardAnalyzer, StandardTokenizer
 from org.apache.lucene.analysis.tokenattributes import CharTermAttribute
+from collections import defaultdict
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
+
+# indexer
+def indextionization(step_one):
+    index = defaultdict(list)
+    for i, tokens in enumerate(step_one):
+        for token in tokens:
+            index[token].append(i)
+    return index
+
+
 # tokenizer
-tokenized = []
-file = open('query.ohsu.1-63', 'r')
-lines = file.readlines()
-for line in lines:
-    if line.startswith("<title>"):
-        test = line[8:].strip()
-        tokenizer = StandardTokenizer()
-        tokenizer.setReader(StringReader(test))
-        charTermAttrib = tokenizer.getAttribute(CharTermAttribute.class_)
-        tokenizer.reset()
-        tokens = []
-        while tokenizer.incrementToken():
-            tokens.append(charTermAttrib.toString())
-        tokenized.append(tokens)
-print(tokenized)
+def tokenization(file):
+    tokenized = []
+    lines = file.readlines()
+    for line in lines:
+        if line.startswith("<title>"):
+            test = line[8:].strip()
+            tokenizer = StandardTokenizer()
+            tokenizer.setReader(StringReader(test))
+            char = tokenizer.getAttribute(CharTermAttribute.class_)
+            tokenizer.reset()
+            tokens = []
+            while tokenizer.incrementToken():
+                tokens.append(char.toString())
+            tokenized.append(tokens)
+    return tokenized
 
 
+def main():
+    file = open('query.ohsu.1-63', 'r')
+    step_one = tokenization(file)
+    print(step_one)
+    step_two = indextionization(step_one)
+    print(step_two)
 
 
-
+if __name__ == "__main__":
+    main()
 
 
 
