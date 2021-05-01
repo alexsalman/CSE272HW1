ac45d623a39d82868183a7c9432979e8d7cc0ff7
Alex Salman <aalsalma@ucsc.edu>
2021-04-30 20:23:36 -0700

    reading <title> lines

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
 # Running Pylucene using docker
 import lucene
-lucene.initVM()
-print(lucene.VERSION)
+from java.io import StringReader
+from org.apache.lucene.analysis.standard import StandardAnalyzer, StandardTokenizer
+from org.apache.lucene.analysis.tokenattributes import CharTermAttribute
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+# tokenizer
+file = open('query.ohsu.1-63', 'r')
+lines = file.readlines()
+for line in lines:
+    if line.startswith("<title>"):
+        print(line.strip())
+
+test = (file.read())
+tokenizer = StandardTokenizer()
+tokenizer.setReader(StringReader(test))
+charTermAttrib = tokenizer.getAttribute(CharTermAttribute.class_)
+tokenizer.reset()
+tokens = []
+while tokenizer.incrementToken():
+    tokens.append(charTermAttrib.toString())
+print(tokens)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# StandardAnalyzer example.
+# analyzer = StandardAnalyzer()
+# stream = analyzer.tokenStream("", StringReader(test))
+# stream.reset()
+# tokens = []
+# while stream.incrementToken():
+#     tokens.append(stream.getAttribute(CharTermAttribute.class_).toString())
+# print(tokens)
