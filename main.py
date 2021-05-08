e2a01e3a1c9708d4cb158dcb5d8f77185a70c1c6
Alex Salman <aliayadalsalman@gmail.com>
2021-05-07 21:41:42 -0700

    updated main
    opening files

--- main.py
+++ main.py
@@ -1,65 +1,15 @@
-# Running Pylucene using docker
-import lucene
-from java.io import StringReader
-from org.apache.lucene.analysis.standard import StandardAnalyzer, StandardTokenizer
-from org.apache.lucene.analysis.tokenattributes import CharTermAttribute
-from collections import defaultdict
-lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+#Running Pylucene using docker
 
-
-# indexer
-def indextionization(step_one):
-    index = defaultdict(list)
-    for i, tokens in enumerate(step_one):
-        for token in tokens:
-            index[token].append(i)
-    return index
-
-
-# tokenizer
-def tokenization(file):
-    tokenized = []
-    lines = file.readlines()
-    for line in lines:
-        if line.startswith("<title>"):
-            test = line[8:].strip()
-            tokenizer = StandardTokenizer()
-            tokenizer.setReader(StringReader(test))
-            char = tokenizer.getAttribute(CharTermAttribute.class_)
-            tokenizer.reset()
-            tokens = []
-            while tokenizer.incrementToken():
-                tokens.append(char.toString())
-            tokenized.append(tokens)
-    return tokenized
+import index
+import search
 
 
 def main():
-    file = open('query.ohsu.1-63', 'r')
-    step_one = tokenization(file)
-    print(step_one)
-    step_two = indextionization(step_one)
-    print(step_two)
+    documents_file = open('ohsumed.88-91', 'r')
+    queries_file = open('query.ohsu.1-63', 'r')
+    directory, analyzer = index.indexer(documents_file)
+    search.searcher(directory, analyzer, queries_file)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-# StandardAnalyzer example.
-# analyzer = StandardAnalyzer()
-# stream = analyzer.tokenStream("", StringReader(test))
-# stream.reset()
-# tokens = []
-# while stream.incrementToken():
-#     tokens.append(stream.getAttribute(CharTermAttribute.class_).toString())
-# print(tokens)
