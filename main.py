0aa06e43ab2a061b5227e0eb16ef76cf04343a6d
Alex Salman <aliayadalsalman@gmail.com>
2021-05-09 19:02:53 -0700

    Final!

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-#Running Pylucene using docker
+#Running Pylucene using docker and PyCharm
 
 import index
 import search
 
 
 def main():
+    # open documents and queries files
     documents_file = open('ohsumed.88-91', 'r')
     queries_file = open('query.ohsu.1-63', 'r')
+    # record documents on the RAM directory
     directory, analyzer = index.indexer(documents_file)
+    # search queries in the documents
     search.searcher(directory, analyzer, queries_file)
+    # closing files used in the algorithm
+    documents_file.close()
+    queries_file.close()
 
 
 if __name__ == "__main__":
